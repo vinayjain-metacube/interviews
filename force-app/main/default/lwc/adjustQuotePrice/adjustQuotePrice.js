82251548f146f953a93df53e967580d428d5c484
Ian Gallagher <ian@provusinc.com>
2023-02-10 10:31:31 -0800

    chore: adding basic core components

---
+++ force-app/main/default/lwc/adjustQuotePrice/adjustQuotePrice.js
@@ -0,0 +1,11 @@
+/*
+ * Provus Services Quoting
+ * Copyright (c) 2023 Provus Inc. All rights reserved.
+ */
+
+import { LightningElement } from "lwc";
+
+export default class AdjustQuotePrice extends LightningElement {
+  adjustedAmountLabel = "Adjusted Amount";
+  adjustedAmount = 0;
+}
