01721ac11faa93af04221ec9c0d21ba5c8d26ba0
Vinay <vinay.jain@metacube.com>
2023-02-23 12:04:10 +0530

    Quote Adjusted Code

--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -3,14 +3,29 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire, track } from "lwc";
+import getQuoteRecord from '@salesforce/apex/QuoteController.getQuoteRecord';
+
 
 export default class EditQuote extends LightningElement {
   @api recordId;
-  quoteData = {
-    name: "Quote Name",
-    endDate: 1547250828000
-  };
+  @track quoteData;
+
+  @wire(getQuoteRecord)
+  wiredData({ error, data }) {
+    if (data) {
+      console.log('Data', data);
+      this.quoteData.name = data.name;
+      this.quoteData.startDate = data.startDate;
+      this.quoteData.endDate = data.endDate;      
+    } else if (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  handleClick(){
+    console.log('Click');
+  }
 
   renderedCallback() {}
 }
